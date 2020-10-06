2361dbd25309e3d3f6d7267a6af82642cae5feaf
ghana7989 <ghana.chindukuri@gmail.com>
2020-10-06 16:40:53 +0530

    Updates

---
+++ precache-manifest.6d6a496f8edfb5930b9c466ef2c74406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791fe7c467c08ff383d009d9371668bc",
+    "url": "/netflix/index.html"
+  },
+  {
+    "revision": "83645cf24b1a870960f5",
+    "url": "/netflix/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "83645cf24b1a870960f5",
+    "url": "/netflix/static/js/2.a00fade2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/netflix/static/js/2.a00fade2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0424e40270be37a7b450",
+    "url": "/netflix/static/js/main.940e80dd.chunk.js"
+  },
+  {
+    "revision": "0587df36741fdadb7899",
+    "url": "/netflix/static/js/runtime-main.5311aa9e.js"
+  }
+]);
